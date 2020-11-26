503227ae5a64cb5355f2cbe8ee77c05b0fd49d45
Mihocsa Szilard <mihocsa48@gmail.com>
2020-11-26 18:46:43 +0100

    customEvent/eventEmitter/ngFor

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
@@ -10,10 +10,22 @@ export class CourseCardComponent implements OnInit {
 
   @Input()
   course:Course;
+ 
+  @Input()
+  cardIndex: number;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>();
   
-  constructor() { }
+  constructor() { };
 
   ngOnInit(): void {
+  }
+
+  onCourseViewed(){
+    console.log("card component - button clicked...");
+    
+    this.courseSelected.emit(this.course);
   }
 
 }
